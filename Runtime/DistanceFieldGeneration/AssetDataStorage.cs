419279b5a5ec5c23139ad9e5c589a0889e341b16
Henning Steinbock <henning@triband.net>
2022-02-27 19:54:50 +0100

    Create AssetDataStorage.cs, but also introduced a crash, probably due out of bounds memory fuckery

---
+++ Runtime/DistanceFieldGeneration/AssetDataStorage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace henningboat.CubeMarching.Runtime.DistanceFieldGeneration
+{
+    public unsafe struct AssetDataStorage : IDisposable
+    {
+        private NativeList<byte> DataBuffer;
+        private NativeList<AssetEntry> Assets;
+
+        public AssetDataStorage(Allocator allocator)
+        {
+            DataBuffer = new NativeList<byte>(allocator);
+            Assets = new NativeList<AssetEntry>(allocator);
+        }
+
+        public T* GetData<T>(int addressInsideDataBuffer) where T : unmanaged
+        {
+            return (T*) DataBuffer.GetUnsafeReadOnlyPtr() + (ulong) addressInsideDataBuffer;
+        }
+
+        public T* CreateAsset<T>(int instanceID, int dataBufferSize) where T : unmanaged
+        {
+            var assetEntry = new AssetEntry(instanceID, DataBuffer.Length);
+            Assets.Add(assetEntry);
+
+            var totalSize = sizeof(T) + dataBufferSize;
+            DataBuffer.Capacity += totalSize;
+            DataBuffer.Length += totalSize;
+
+            return (T*) DataBuffer.GetUnsafePtr() + (ulong) assetEntry.ByteOffsetInDataBuffer;
+        }
+
+        public void Dispose()
+        {
+            if (DataBuffer.IsCreated) DataBuffer.Dispose();
+            if (Assets.IsCreated) Assets.Dispose();
+        }
+
+        private readonly struct AssetEntry
+        {
+            public readonly int InstanceID;
+            public readonly int ByteOffsetInDataBuffer;
+
+            public AssetEntry(int instanceID, int byteOffsetInDataBuffer)
+            {
+                InstanceID = instanceID;
+                ByteOffsetInDataBuffer = byteOffsetInDataBuffer;
+            }
+        }
+    }
+}
